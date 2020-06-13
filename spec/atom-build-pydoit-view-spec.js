b4e27e3255df646bb82bcf259c66e49eeb31a3c9
Rémi Berthoz <remi.berthoz@gmail.com>
2020-06-13 21:02:44 +0200

    Initial commit: generate atom package

---
+++ spec/atom-build-pydoit-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomBuildPydoitView from '../lib/atom-build-pydoit-view';
+
+describe('AtomBuildPydoitView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
