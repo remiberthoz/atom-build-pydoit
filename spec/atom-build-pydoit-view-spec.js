583c4b36834d307a8a9bdcae1efad2ac0051800d
Rémi Berthoz <remi.berthoz@gmail.com>
2020-06-14 14:40:35 +0200

    refactor: remove unused files from atom package generation

--- spec/atom-build-pydoit-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import AtomBuildPydoitView from '../lib/atom-build-pydoit-view';
-
-describe('AtomBuildPydoitView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
